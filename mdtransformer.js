84c3f1de1b4b527285ac486af3b91b659f0405db
Maksym Rybalko <maxymrybalko2005@gmail.com>
2024-03-05 18:44:14 +0200

    Minor regex change

--- mdtransformer.js
+++ mdtransformer.js
@@ -8,15 +8,15 @@ const { dirname } = require('path');
 const options = program.opts();
 const preformatted = [];
 const tags = [
-  { pattern: /(?<=[ ,.:;\n\t]|^)\*\*(?=\S)(.+?)(?<=\S)\*\*(?=[ ,.:;\n\t]|$)/g,
+  { pattern: /(?<=[ ,.:;>\n\t]|^)\*\*(?=\S)(.+?)(?<=\S)\*\*(?=[ ,.:;<\n\t]|$)/g,
     replacement: '<b>$1</b>',
   },
   {
-    pattern: /(?<=[ ,.:;\n\t]|^)_(?=\S)(.+?)(?<=\S)_(?=[ ,.:;\n\t]|$)/g,
+    pattern: /(?<=[ ,.:;>\n\t]|^)_(?=\S)(.+?)(?<=\S)_(?=[ ,.:;<\n\t]|$)/g,
     replacement: '<i>$1</i>',
   },
   {
-    pattern: /(?<=[ ,.:;\n\t]|^)`(?=\S)(.+?)(?=\S)`(?=[ ,.:;\n\t]|$)/g,
+    pattern: /(?<=[ ,.:;>\n\t]|^)`(?=\S)(.+?)(?=\S)`(?=[ ,.:;<\n\t]|$)/g,
     replacement: '<tt>$1</tt>',
   },
 ];
